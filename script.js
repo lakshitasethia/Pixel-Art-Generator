40b202827cc1fb2f63894e7ba6598b72612ca5ce
Lakshita <ksethia1978&gmail.com>
2026-04-12 11:54:24 +0530

    all done!!

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector(".container");
 const sizeEl = document.querySelector(".grid-count");
 const size = sizeEl.value;
 
+const color = document.querySelector(".color-picker");
+const reset = document.querySelector(".reset");
+
+let draw = false;
+
 function populate(size) {
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -10,7 +15,33 @@ function populate(size) {
     const div = document.createElement("div");
     div.classList.add("pixel");
     container.appendChild(div);
+
+    div.addEventListener("mouseover", function () {
+      if (draw) {
+        div.style.backgroundColor = color.value;
+      } else {
+        return;
+      }
+    });
+    div.addEventListener("mousedown", function () {
+      div.style.backgroundColor = color.value;
+    });
   }
 }
+window.addEventListener("mousedown", function () {
+  draw = true;
+});
 
-populate(size);
+window.addEventListener("mouseup", function () {
+  draw = false;
+});
+
+sizeEl.addEventListener("keyup", function() {
+  container.innerHTML = "";
+  populate(sizeEl.value);
+});
+
+reset.addEventListener("click", function () {
+  container.innerHTML = "";
+  populate(sizeEl.value);
+});
