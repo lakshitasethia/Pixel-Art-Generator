56d969a151ceeff7509ddf095c5dc1f5fc99607f
Lakshita <ksethia1978&gmail.com>
2026-04-13 14:18:05 +0530

    used stacks to implement an undo and redo button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.querySelector(".container");
-
 const sizeEl = document.querySelector(".grid-count");
-const size = sizeEl.value;
-
 const color = document.querySelector(".color-picker");
 const reset = document.querySelector(".reset");
+const undoBtn = document.querySelector(".undo");
+const redoBtn = document.querySelector(".redo");
 
 let draw = false;
+const history = [];
+const redoStack = [];
 
 function populate(size) {
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -18,30 +19,61 @@ function populate(size) {
 
     div.addEventListener("mouseover", function () {
       if (draw) {
+        history.push({ div, previousColor: div.style.backgroundColor });
+        redoStack.length = 0; // clear redo whenever new action happens
         div.style.backgroundColor = color.value;
-      } else {
-        return;
       }
     });
+
     div.addEventListener("mousedown", function () {
+      history.push({ div, previousColor: div.style.backgroundColor });
+      redoStack.length = 0;
       div.style.backgroundColor = color.value;
     });
   }
 }
-window.addEventListener("mousedown", function () {
-  draw = true;
+
+window.addEventListener("mousedown", () => (draw = true));
+window.addEventListener("mouseup", () => (draw = false));
+
+undoBtn.addEventListener("click", function () {
+  if (history.length === 0) return;
+  const last = history.pop();
+  redoStack.push({
+    div: last.div,
+    previousColor: last.div.style.backgroundColor,
+  });
+  last.div.style.backgroundColor = last.previousColor;
 });
 
-window.addEventListener("mouseup", function () {
-  draw = false;
+redoBtn.addEventListener("click", function () {
+  if (redoStack.length === 0) return;
+  const last = redoStack.pop();
+  history.push({
+    div: last.div,
+    previousColor: last.div.style.backgroundColor,
+  });
+  last.div.style.backgroundColor = last.previousColor;
 });
-
-sizeEl.addEventListener("keyup", function() {
+window.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && e.shiftKey && e.key === "Z") {
+    redoBtn.click();
+  } else if (e.ctrlKey && e.key === "z") {
+    undoBtn.click();
+  }
+});
+sizeEl.addEventListener("keyup", function () {
   container.innerHTML = "";
+  history.length = 0;
+  redoStack.length = 0;
   populate(sizeEl.value);
 });
 
 reset.addEventListener("click", function () {
   container.innerHTML = "";
+  history.length = 0;
+  redoStack.length = 0;
   populate(sizeEl.value);
 });
+
+populate(sizeEl.value);
